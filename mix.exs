f23b4df1ac3aac042bcfa623c222a1f8c24d157f
Daniel Khaapamyaki <daniel@superlist.com>
2022-03-01 00:11:53 +0100

    docs: Add package information

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Ferryman.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "Pure Elixir JSONRPC2 Client & Server realization for Ruby, Erlang and Elixir",
+      name: "FerrymanEx",
+      source_url: "https://github.com/superlistapp/ferryman_ex"
     ]
   end
 
@@ -24,6 +28,14 @@ defmodule Ferryman.MixProject do
       {:jsonrpc2, "~> 2.0"},
       {:jason, "~> 1.0"},
       {:redix, "~> 1.1"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Daniel Khaapamyaki"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/superlistapp/ferryman_ex"}
     ]
   end
 end
